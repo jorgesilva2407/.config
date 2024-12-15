77a5ef52ea5ad10ad496befadffcc9ad06c5975e
jorgesilva2407 <jorge.lima2407@gmail.com>
2024-12-15 15:22:02 -0300

    configurations

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre',
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    end
+  }
+}
