a4f9056dabb953de5fc82873765ec157b317028f
jorgesilva2407 <jorge.lima2407@gmail.com>
2024-12-15 15:24:10 -0300

    convert soft links into hard links

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,1 +1,15 @@
-.wezterm.lua
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Dracula'
+config.font = wezterm.font('FiraCode Nerd Font')
+config.font_size = 15.0
+config.window_close_confirmation = 'NeverPrompt'
+
+config.keys = {
+  { key = "LeftArrow", mods = "ALT", action = wezterm.action.SendString "\x1bb" },
+  { key = "RightArrow", mods = "ALT", action = wezterm.action.SendString "\x1bf" },
+}
+
+return config
